ea3cd76f3178d7464a1906d1f6470e3e96c1466f
Vojtěch Struhár <vojtechstruhar@gmail.com>
2024-09-09 21:22:56 +0200

    Adjust run comment

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# `love` here is the Love2D executable (installed with homebrew, so it's linked)
+# `love` here is the Love2D executable (installed with homebrew, so it's in path)
 # https://www.love2d.org/wiki/Getting_Started
 
 open -n -a love "/Users/vojtechstruhar/Development/LuaProjects/love-gdungeon"
